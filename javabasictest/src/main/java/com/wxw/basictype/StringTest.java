59c69d40cf6c7f3840a4d8c6e9ccfa618bee47f3
wxw <wangxw@ikongjian.com>
2020-05-18 10:29:32 +0800

    hithub test

---
+++ javabasictest/src/main/java/com/wxw/basictype/StringTest.java
@@ -0,0 +1,14 @@
+package com.wxw.basictype;
+
+/**
+ * @ClassName StringTest
+ * @Description TODO
+ * @Author wangxinwei
+ * @Date 2020/5/18 10:23
+ * @Version 1.0
+ */
+public class StringTest {
+    public static void main(String[] args) {
+
+    }
+}
